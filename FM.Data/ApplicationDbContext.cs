a1c0ad9b02db322c90acb1cea65838e79bc339e3
Tobi2Tobi <20182009@atn.ac.at>
2023-02-02 17:39:59 +0100

    pm.data

---
+++ FM.Data/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using PM.Data.Entity;
+
+namespace PM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Plane> Plane { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Plane>().HasData(
+                new Plane()
+                {
+                    Id = 1,
+                    Name = "Bowing",
+                    Seats = 100,
+                    IsActive = true,
+                },
+                new Plane()
+                {
+                    Id = 2,
+                    Name = "Airbus",
+                    Seats = 300,
+                    IsActive = true,
+                });
+        }
+    }
+}
---
+++ FM.Data/Entity/Plane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Data.Entity
+{
+    public class Plane
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        public int Seats { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? Created { get; set; }
+    }
+}
